e6ab52753a53c1d0281c559af0e4ce2a56d5eade
dhanikprakash <dhanikprakash@gmail.com>
2023-03-03 18:50:47 +0000

    update tours

--- src/components/Pages/Tour.tsx
+++ src/components/Pages/Tour.tsx
@@ -1,7 +1,35 @@
+import { FormControl, Grid, InputLabel, MenuItem, Select } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 const Tour = () => {
-  return <div>Tour</div>;
+  return (<Container>
+    <div>Tour</div>
+    <Grid container>
+      <Grid item xs={3}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            variant="standard"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={10}
+            label="Age"
+            IconComponent={({ ...props }) => { console.log(JSON.stringify({ ...props })); return (<AccessAlarmIcon {...props} sx={{ fill: 'green' }} />) }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={3}>
+        <AccessAlarmIcon />
+      </Grid>
+    </Grid>
+  </Container >);
 };
 
 export default Tour;
